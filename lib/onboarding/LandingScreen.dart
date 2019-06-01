2a0a127fdbf2b02f522522298d5e66015bc79b2b
DammyOlolade <dammyololade2010@gmail.com>
2019-06-01 12:20:07 +0100

    init

---
+++ lib/onboarding/LandingScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+///the main screen where the user starts the journey from
+/// project: flutter_collabo
+/// @package: onboarding
+/// @author dammyololade <damola@kobo360.com>
+/// created on 2019-06-01
+class LandingScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LandingScreenState();
+  }
+
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
